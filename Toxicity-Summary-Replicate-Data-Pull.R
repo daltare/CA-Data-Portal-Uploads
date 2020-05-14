bcf4456221842ffe470c861a5f74f624b7254002
daltare <daltare@gmail.com>
2020-05-13 17:26:25 -0700

    update toxicity files

--- Toxicity-Summary-Replicate-Data-Pull.R
+++ Toxicity-Summary-Replicate-Data-Pull.R
@@ -412,6 +412,8 @@ library(reticulate)
         import('requests')
         import('requests_toolbelt')
         import('datetime')
-        py_run_file("C:\\David\\Open_Data_Project\\__CA_DataPortal\\Toxicity\\Summary-Replicate-Results\\portal-upload-ckan-chunked_Tox\\main_Tox.py")
+        #py_run_file("C:\\David\\Open_Data_Project\\__CA_DataPortal\\Toxicity\\Summary-Replicate-Results\\portal-upload-ckan-chunked_Tox\\main_Tox.py")
+        py_run_file("C:\\David\\Open_Data_Project\\__CA_DataPortal\\Toxicity\\Summary-Replicate-Results\\portal-upload-ckan-chunked_Tox\\main_Tox_Summary.py")
+        py_run_file("C:\\David\\Open_Data_Project\\__CA_DataPortal\\Toxicity\\Summary-Replicate-Results\\portal-upload-ckan-chunked_Tox\\main_Tox_Replicate.py")
                 
 }
