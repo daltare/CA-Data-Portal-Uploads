7b7b54e247f03e9cb8fc6aae0f6f8857d015d991
daltare <daltare@gmail.com>
2021-02-09 10:09:17 -0800

    add script to create ceden parquet files

---
+++ ceden_arrow_parquet_conversion.r
@@ -0,0 +1,180 @@
+#####################################################################################
+# Resources: 
+    # https://gist.github.com/jthomasmock/b8a1c6e90a199cf72c6c888bd899e84e
+#####################################################################################
+
+#### SETUP #######################################################################
+# load packages
+library(arrow)
+library(tidyverse)
+library(tictoc)
+library(readr)
+library(data.table)
+library(glue)
+library(readxl)
+library(zip)
+
+# enter some information and create a parent directory
+download_date <- '2021-02-08'
+list_datasets <- list('ceden_chemistry' = c(file_name = 'WaterChemistryData',
+                                            data_dictionary = 'water_chemistry\\CEDEN_Chemistry_Data_Dictionary.xlsx'),
+                      'ceden_tissue' = c(file_name = 'TissueData',
+                                         data_dictionary = 'tissue\\CEDEN_Tissue_Data_Dictionary.xlsx'),
+                      'ceden_habitat' = c(file_name = 'HabitatData',
+                                          data_dictionary = 'habitat\\CEDEN_Habitat_Data_Dictionary.xlsx'),
+                      'ceden_benthic' = c(file_name = 'BenthicData',
+                                          data_dictionary = 'benthic\\CEDEN_Benthic_Data_Dictionary.xlsx'),
+                      'ceden_toxicity' = c(file_name = 'ToxicityData',
+                                           data_dictionary = 'toxicity\\CEDEN_Toxicity_Data_Dictionary.xlsx'))
+data_dictionaries_path <- 'C:\\David\\Open_Data_Project\\__CA_DataPortal\\CEDEN\\data_dictionary_conversion'
+data_files_path <- 'C:\\David\\Open_Data_Project\\__CA_DataPortal\\CEDEN\\data_download\\CEDEN_Datasets'
+
+
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# create function to create a parquet file for data from 2000 - present
+# create parquet file for for a single year, given the year, directory name for the new file, and file name for the source data
+convert_data <- function(year, directory_name, file_name) {
+    source_file <- glue('{data_files_path}\\{download_date}\\{file_name}_year-{year}_{download_date}.csv')
+    # check to see if source data file exists, if so create a corresponding parquet file
+    if (file.exists(source_file)) {
+        # create directory for the given year
+        dir.create(file.path(directory_name, year))
+        
+        # read source data file
+        df_ceden <- read_csv(glue('{data_files_path}\\{download_date}\\{file_name}_year-{year}_{download_date}.csv'), 
+                             col_types = field_types, 
+                             na = 'NaN')
+        
+        # create parquet file
+        arrow::write_parquet(df_ceden, glue('{directory_name}/{year}/data.parquet'))
+    }
+}
+
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# create function to create a parquet file for each year with data prior to 2000
+# create parquet file for for a single year, given the year, directory name for the new file, and data frame with source data
+    convert_data_pre2000 <- function(year, directory_name, source_data){
+        # create directory for the given year
+        dir.create(file.path(directory_name, year))
+        
+        # filter source data for given year
+        df_ceden_year <- source_data %>% 
+            filter(sample_year == year) %>% 
+            select(-sample_year)
+        
+        # create parquet
+        arrow::write_parquet(df_ceden_year, 
+                             glue('{directory_name}/{year}/data.parquet'))
+    }
+
+    
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# loop through all of the datasets and create parquet files
+for (i in seq_along(names(list_datasets))) {
+    directory_name <- names(list_datasets[i])
+    directory_name <- glue('{directory_name}_{download_date}')
+    # create a directory for the given data type
+    dir.create(directory_name)
+    latest_year <- year(as.Date(download_date))
+    file_name <- list_datasets[[i]][['file_name']]
+    
+    # create a list of the column types, to use when reading in the data
+    df_types <- read_xlsx(glue('{data_dictionaries_path}\\{list_datasets[[i]][["data_dictionary"]]}')) #water_chemistry\\CEDEN_Chemistry_Data_Dictionary.xlsx")
+    field_types <- df_types %>% 
+        pull(type) %>% 
+        str_replace(pattern = 'text', replacement = 'c') %>% 
+        str_replace(pattern = 'numeric', replacement = 'n') %>% 
+        str_replace(pattern = 'timestamp', replacement = 'T') %>% 
+        glue_collapse() %>%  
+        {.}
+    
+    #### create folder and parquet file for each year from 2000 to present ####
+        walk(2000:latest_year, ~ convert_data(., directory_name, file_name))
+        
+    #### pre-2000 data ####
+    # read file
+    df_ceden_pre2000 <- read_csv(glue('{data_files_path}\\{download_date}\\{file_name}_prior_to_2000_{download_date}.csv'), 
+                                      col_types = field_types, 
+                                      na = 'NaN') %>% 
+        mutate(sample_year = year(SampleDate))
+    
+    # get a list of years in the dataset
+    list_years_pre2000 <- df_ceden_pre2000 %>% 
+        distinct(sample_year) %>% 
+        arrange(sample_year) %>% 
+        pull(sample_year)
+    
+    # create folder and parquet file for each year
+        walk(list_years_pre2000, ~ convert_data_pre2000(., directory_name, df_ceden_pre2000))
+        rm(df_ceden_pre2000)
+        
+    # add all of the files to a zip file, but without compression (this file can be loaded to the data portal)
+    zip::zip(zipfile = glue('{directory_name}.zip'), 
+             root = directory_name,
+             recurse = TRUE,
+             #mode = 'cherry-pick',
+             # files = glue('{directory_name}/{list.files(directory_name, recursive = TRUE)}'),
+             files = list.files(recursive = TRUE),
+             compression_level = 0)
+    
+    # move the zip file back to the working directory
+    if (file.copy(from = glue('{directory_name}/{directory_name}.zip'), to = '.')) {
+        unlink(glue('{directory_name}/{directory_name}.zip'))
+    }
+}
+    
+
+    
+#### TEST ############################################################################
+# open connection as arrow
+    directory_name_chem <- names(list_datasets[1])
+    directory_name_chem <- glue('{directory_name_chem}_{download_date}')
+    ds_chemistry <- open_dataset(directory_name_chem, partitioning = "year")
+
+# pull some data
+    tic()
+    df_from_arrow_all <- ds_chemistry %>% 
+        select(year, Analyte, Unit, Result, MDL, RL) %>%
+        filter(Analyte %in% c("E. coli")) %>% 
+        collect() 
+    toc()
+    
+    df_from_arrow_all %>% count(Analyte)
+    View(df_from_arrow_all %>% count(year))
+
+# pull some data then summarize
+    tic()
+    df_from_arrow_all_summarize <- ds_chemistry %>% 
+        select(year, Analyte, Unit, Result, MDL, RL) %>%
+        filter(Analyte %in% c("E. coli")) %>% 
+        # collect() %>% 
+        group_by(Analyte, Unit, year) %>%
+        collect() %>% 
+        summarize(
+            avg_result = mean(Result, na.rm = TRUE),
+            max_result = max(Result, na.rm = TRUE),
+            n = n()
+        ) %>%
+        {.}
+    toc()
+    df_from_arrow_all_summarize
+
+# get number of records by year
+    tic()
+    df_from_arrow_all_counts <- ds_chemistry %>% 
+        select(year, Analyte, Unit, Result, MDL, RL) %>%
+        # filter(Analyte %in% c("E. coli")) %>% 
+        collect() %>%
+        count(year) %>% 
+        # collect() %>% 
+        # summarize(
+        #     avg_result = mean(Result, na.rm = TRUE),
+        #     max_result = max(Result, na.rm = TRUE),
+        #     n = n()
+        # ) %>%
+        {.}
+    toc()
+    View(df_from_arrow_all_counts)
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
