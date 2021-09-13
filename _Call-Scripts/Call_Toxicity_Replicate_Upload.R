090c43948e619aed92241f7c1bd519b12003add7
daltare <daltare@gmail.com>
2021-09-13 13:40:30 -0700

    add error handling / automated email to Toxicity summary/replicate script

--- _Call-Scripts/Call_Toxicity_Replicate_Upload.R
+++ _Call-Scripts/Call_Toxicity_Replicate_Upload.R
@@ -4,4 +4,4 @@
 # this file, and set the time/date options (make sure the date format is %m/%d/%Y)
 
 # source('C:/David/Open_Data_Project/__CA_DataPortal/Toxicity/Summary-Replicate-Results/Toxicity-Summary-Replicate-Data-Pull.R', chdir = TRUE) # runs the program to upload a csv file to the portal
-reticulate::py_run_file('C:\\Users\\daltare\\OneDrive - Water Boards\\projects\\CA_data_portal\\Toxicity\\portal-upload-ckan-chunked_Tox\\main_Tox_Replicate.py')
+# reticulate::py_run_file('C:\\Users\\daltare\\OneDrive - Water Boards\\projects\\CA_data_portal\\Toxicity\\portal-upload-ckan-chunked_Tox\\main_Tox_Replicate.py')
