49c42c64d5036b10b21f6617f649934e93091403
Altare <David.Altare@Waterboards.ca.gov>
2021-10-15 13:39:47 -0700

    updated SMARTS script to download chrome drivers before matching drivers/versions

--- SMARTS/SMARTS_data_portal_automation.R
+++ SMARTS/SMARTS_data_portal_automation.R
@@ -259,6 +259,7 @@ tryCatch(
                      c("-Dwebdriver.chrome.verboseLogging=true"), 
                  retcommand = TRUE,
                  port = port_use)
+        Sys.sleep(5)
         
         #### get current version of chrome browser ----
         chrome_browser_version <-
@@ -281,6 +282,16 @@ tryCatch(
             as.numeric_version() %>%
             max() %>%
             as.character()
+        
+        #### re-check for open port ----
+        for (port_check in 4567L:4577L) {
+            port_test <- ping_port(destination = 'localhost', port = port_check)
+            # print(all(is.na(port_test)))
+            if (all(is.na(port_test)) == TRUE) {
+                port_use <- port_check
+                break
+            }
+        }
         
         #### set up selenium with the current chrome version ----
         selCommand <- selenium(jvmargs = 
@@ -698,181 +709,6 @@ tryCatch(
         stop(e)
     }
 )
-
-
-
-# # create combined files - NOTE: Might want to first use facility info from active facilities, then use info from inactive facilities for those that don't get a match (some sites are listed twice in the facilities info files, one active and one inactive)
-#     # Facilities
-#         ind.facilities <- read_csv(paste0(dataset_list$dataset2$filename, '_', Sys.Date(), '.csv'), guess_max = 50000)
-#         ind.results <- read_csv(paste0(dataset_list$dataset1$filename, '_', Sys.Date(), '.csv'), guess_max = 50000)
-#         con.facilities <- read_csv(paste0(dataset_list$dataset4$filename, '_', Sys.Date(), '.csv'), guess_max = 50000)
-#         con.results <- read_csv(paste0(dataset_list$dataset3$filename, '_', Sys.Date(), '.csv'), guess_max = 50000)
-# 
-# 
-#         ind.facilities <- ind.facilities %>% select(WDID, REGION_BOARD, COUNTY, FACILITY_LATITUDE, FACILITY_LONGITUDE, PERMIT_TYPE, APP_ID, STATUS)
-#         con.facilities <- con.facilities %>% select(WDID, REGION, COUNTY, SITE_LATITUDE, SITE_LONGITUDE, PERMIT_TYPE, APP_ID, STATUS)
-#         names(ind.facilities) <- gsub(pattern = 'FACILITY_', replacement = '', x = names(ind.facilities))
-#         names(ind.facilities) <- gsub(pattern = '_BOARD', replacement = '', x = names(ind.facilities))
-#         names(con.facilities) <- gsub(pattern = 'SITE_', replacement = '', x = names(con.facilities))
-#         all.facilities <- bind_rows(ind.facilities, con.facilities)
-#         write_csv(x = all.facilities, path = paste0('All_Facilities_', Sys.Date(), '.csv'))
-#         rm(list = c('ind.facilities', 'con.facilities'))
-# 
-#     # Industrial
-#         # first join just the active facility info
-#         ind.active <- ind.facilities %>% filter(STATUS == 'Active')
-#         ind.terminated <- ind.facilities %>% filter(STATUS == 'Terminated')
-#         ind.combined <- left_join(x = ind.results, y = ind.active)
-#         ind.combined.matched <- ind.combined %>% filter(!is.na(ind.combined$STATUS))
-#         ind.notMatched <- ind.results[is.na(ind.combined$STATUS),]
-#         ind.combined.notMatched <- left_join(x = ind.notMatched, y = ind.facilities)
-#         ind.combined.final <- bind_rows(ind.combined.matched, ind.combined.notMatched)
-#         write_csv(x = ind.combined.final, path = paste0('Industrial_Combined_', Sys.Date(), '.csv'))
-#         # simple join
-#             z <- left_join(ind.results %>% distinct(), ind.facilities %>% group_by(WDID) %>% select(-PERMIT_TYPE, -APP_ID) %>% distinct())
-#         # compare
-#             identical(ind.combined.final, z)
-#             nrow(z)
-#             nrow(ind.combined.final)
-# 
-#     # Construction
-#         con.combined <- left_join(x = con.results, y = con.facilities, by = c('WDID'))
-#         write_csv(x = con.combined, path = paste0('Construction_Combined_', Sys.Date(), '.csv'))
-
-
-# # Download Report data (datasets available by clicking on a 'Export to Excel' button)
-#     # define a function to download a given dataset from the reports pages
-#         SMARTS_form_download <- function(reports_page_id, report_id, run_report_id, export_excel_id, smarts_file_name, smarts_file_type,
-#                                          filename, portal_node, delete_old_versions = FALSE) { # NOTE: To find the html identifiers of the button/link in the html code use the 'developer' tool in the browser
-#             # this automatically downloads the file to the default download location for the browser, set above
-#             # NOTE: Files downloaed from SMARTS reports are automatically named sample.xls, so check to make sure there isn't already an un-named file (sample.xls) in 
-#             # this location  - if so, delete it (otherwise the newly downloaded file will be appended with a number, and the versions might get confused)
-#             if (file.exists(smarts_file_name)) {
-#                 unlink(smarts_file_name)
-#             }
-#             # Open the link to the relevant reports page 
-#                 webElem <- remDr$findElement(using = 'id', value = reports_page_id)
-#                 webElem$clickElement()
-#                 allWins <- unlist(remDr$getWindowHandles())
-#                 reportWindow <- allWins[!allWins %in% homeWindow[[1]]]
-#                 # Switch to the reports page
-#                     # remDr$switchToWindow(reportWindow) # this no longer works... need to use the custom function below, from: https://github.com/ropensci/RSelenium/issues/143
-#                     
-#                     myswitch <- function (remDr, windowId) {
-#                         qpath <- sprintf("%s/session/%s/window", remDr$serverURL,
-#                                          remDr$sessionInfo[["id"]])
-#                         remDr$queryRD(qpath, "POST", qdata = list(handle = windowId))
-#                     }
-#                     myswitch(remDr = remDr, windowId = reportWindow[[1]])
-#             # select the report link
-#                 webElem <- remDr$findElement(using = 'id', value = report_id)
-#                 webElem$clickElement()
-#             # select the run report button
-#                 webElem <- remDr$findElement(using = 'id', value = run_report_id)
-#                 webElem$clickElement()
-#                 # Alt Method
-#                 # NOTE: It's also possible to do this 'manually' (i.e., by recreating the mouse actions and button clicks), like this:
-#                 loc <- webElem$getElementLocation()
-#                 remDr$mouseMoveToLocation(webElement = webElem)
-#                 remDr$click()
-# 
-#             # select the Export To Excel button
-#                 webElem <- remDr$findElement(using = 'id', value = export_excel_id)
-#                 webElem$clickElement()
-#             # pause until the file finishes downloading
-#             i <- 1
-#             while (!file.exists(smarts_file_name) & i <= 300) { # check to see if the file exists (but only up to 300 times)
-#                 Sys.sleep(time = 1) # if the file doesn't exist yet, wait 1 second then check again
-#                 i <- i + 1 # to keep track of how many times the loop runs, to prevent an infinite loop
-#             }
-#             # Rename the file, and append with the date for easier identification (may want to add in the time too?)
-#                 file.rename(from = smarts_file_name, to = paste0(filename, '-', Sys.Date(), '.', smarts_file_type))
-#             # to add the time to the filename
-#                 # file.rename(from = 'file.txt', to = paste0('Industrial_Ad_Hoc_Reports_-_Parameter_Data_', Sys.Date(),'_', hour(Sys.time()),'.', minute(Sys.time()), '.', if(am(Sys.time())) {'AM'} else {'PM'}))
-#             # Delete old versions of the files (if desired)
-#             if (delete_old_versions == TRUE) {
-#                 files.list <- grep(pattern = paste0('^', filename), x = list.files(), value = TRUE) # get a list of all of the files of this type (including the new one) (NOTE: ^ means: starts with..)
-#                 files.list.old <- files.list[files.list != paste0(filename, '_', Sys.Date(), '.xls')] # exclude the new file from the list of files to be deleted
-#                 file.remove(files.list.old)
-#             }
-#             # convert the file to .csv
-#                 report.result <- readHTMLTable(paste0(filename, '_', Sys.Date(), '.xls'))
-#                 # report.result <- list.clean(report.result, fun = is.null, recursive = FALSE)
-#                 # n.rows <- unlist(lapply(report.result, function(t) dim(t)[1]))
-#                 report.result <- report.result[[1]]
-#                 # check last row (may be blank)
-#                     check.last.row <- apply(report.result[nrow(report.result),], 2, function(x) {x == ''})
-#                     if (sum(!check.last.row) == 0) {
-#                         report.result <- report.result[-nrow(report.result), ]
-#                     }
-#                 # write to csv
-#                     # NEED TO CHECK FOR DOUBLE SPACES, ETC BEFORE WRITING FILE
-#                     for (i in 1:3) {
-#                         report.result <- apply(X = report.result, MARGIN = 2, FUN = function(x) {gsub(pattern = '  ', replacement = ' ', x = x)})
-#                         report.result <- as.data.frame(report.result)
-#                     }
-#                     report.result <- as_tibble(report.result)
-#                     report.result <- clean_names(report.result)
-#                 # Write File
-#                     write_csv(x = report.result, path = paste0(filename, '_', Sys.Date(), '.csv'))
-#                     
-#             # # NOTE - DOESN'T LOOK LIKE THIS PART IS NEEDED
-#             # # Read the data as text strings into R (so that it can be checked for special characters, etc)
-#             #     write_tsv(x = report.result, path = paste0(filename, '_', Sys.Date(), '_Raw.txt'))
-#             #     dataset_lines <- readLines(paste0(filename, '_', Sys.Date(), '_Raw.txt'))
-#             # # Check for quotes, and remove them (to avoid problems with special characters)    
-#             #     problems <- grep(pattern = '\"*\"', x = dataset_lines) # this finds lines with quoted text - used below to remove the quotes ("t" is especially problematic, because it can get confused with a tab delimiter)
-#             #     for (i in seq(length(problems))) {
-#             #         dataset_lines[problems[i]] <- gsub(pattern = '\"*\"', replacement = '', x = dataset_lines[problems[i]]) # this removes the quotes (but keeps the text within the quotes)
-#             #     }
-#             # # Make sure all encoding is in UTF-8
-#             #     dataset_lines <- iconv(x = dataset_lines, to = 'UTF-8') # encoding options available with: stri_enc_list(simplify = TRUE)
-#             #     dataset_lines <- str_conv(string = dataset_lines, encoding = 'UTF-8')
-#             # # # check
-#             # #     table(Encoding(dataset_lines))
-#             # #     table(stri_enc_mark(dataset_lines))
-#             # #     table(stri_enc_isutf8(dataset_lines))
-#             # # write the corrected dataset to a temporary file
-#             #     t <- tempfile()
-#             #     writeLines(text = dataset_lines, con = file(t, encoding = 'UTF-8'), sep = '\n')
-#             # # read the new dataset, then close the temporary file
-#             #     dataset <- suppressMessages(read_tsv(file = t, guess_max = 10000))
-#             #     unlink(t)
-#             # # write the corrected dataset to a pipe delimited txt file
-#             #     write_csv(x = dataset, path = paste0(filename, '_', Sys.Date(), '_2.csv'))
-#             #     rm(dataset_lines, problems, dataset, t)
-#             # # check
-#             #     # z_check_1 <- read_csv(paste0(filename, '_', Sys.Date(), '.csv'), guess_max = 10000)
-#             #     # z_check_2 <- read_csv(paste0(filename, '_', Sys.Date(), '_2.csv'), guess_max = 10000)
-#             #     # identical(z_check_1, z_check_2)
-#             
-#                 
-#                 
-#             # close the reports window
-#                 remDr$closeWindow()
-#                 # remDr$switchToWindow(homeWindow[[1]])
-#                 myswitch(remDr = remDr, windowId = homeWindow[[1]])
-#         }
-
-
-
-
-
-
-
-
-# # Download the files
-#     for (i in seq(length(forms_datasets_list))) {
-#         SMARTS_form_download(reports_page_id = forms_datasets_list[[i]]$reports_page_id,
-#                              report_id = forms_datasets_list[[i]]$report_id,
-#                              run_report_id = forms_datasets_list[[i]]$run_report_id,
-#                              export_excel_id = forms_datasets_list[[i]]$export_excel_id,
-#                              smarts_file_name = forms_datasets_list[[i]]$smarts_file_name,
-#                              smarts_file_type = forms_datasets_list[[i]]$smarts_file_type,
-#                              filename = forms_datasets_list[[i]]$filename,
-#                              portal_node = forms_datasets_list[[i]]$portal_node,
-#                              delete_old_versions = delete_old_versions)
-#     }
 
 
 
